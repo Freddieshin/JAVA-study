8595655ce6372729393976d91ae1d067bdcd1204
Freddieshin <sjm6624@naver.com>
2024-05-30 09:34:37 +0900

    init

---
+++ src/Test.java
@@ -0,0 +1,15 @@
+public class Test {
+
+//
+    /**
+     * @author 신재민
+     * @since 2024.05.28
+     */
+
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+    }
+}

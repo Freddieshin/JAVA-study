e148f04281e37235f5e2facda9a1ed81d323c722
Freddieshin <sjm6624@naver.com>
2024-05-30 17:18:23 +0900

    오늘 강의에서 배운 타입 및 퀴즈실습

--- src/Test.java
+++ src/Test.java
@@ -1,15 +1,92 @@
 public class Test {
+    public static void main(String[] args){
 
-//
-    /**
-     * @author 신재민
-     * @since 2024.05.28
-     */
+        }
+      }
+
+  /**
+    @author 신재민
+    @since 2024.05.28
+   /
+  // 아래구문은 메소드입니다
+
+   public static void main(String[] args) {
+    {타입;} 변수명 = 값;
+    int a = 10;
+    System.out.println(a);
+   }
 
     public static void main(String[] args) {
-        int a = 10;
-        System.out.println(a);
-        System.out.println(a);
-        System.out.println(a);
+    int a = 25;
+    int b = 25;
+    int c = 50;
+    System.out.println(a+b+c);
     }
-}
+
+    public static void main(String[] args){
+    int a = 1;
+    byte b = 2;
+    long c = 3;
+    short d = 4;
+    // 정수형
+    float e = 0.1f;
+    //    float는 값뒤에 f가 붙어야함
+    double f = 0.2;
+    // 실수형
+    char a = 'a';
+    // 문자형  값안에 ' '감싸는 문자임
+    boolean istrue = true;
+    boolean isfalse = false;
+    // 불리언형
+    String str = "hello world";
+    // 문자열형
+    ---------
+    int a = 2147483648;
+    int 타입은 2147483647 까지 표현할수 있음으로 위에 예제는 출력불가
+    }
+    ---------
+    public static void main(String[] args){
+    char c1 = 'A';
+    char c2 = 65;
+    char c3 = '\u0041';
+
+    char c4 = '가';
+    char c5 = 44032;
+    char c6 = '\uac00';
+
+    int uniCode = c1;
+
+    System.out.println(c1);
+    System.out.println(c2);
+    System.out.println(c3);
+    System.out.println(c4);
+    System.out.println(c5);
+    System.out.println(c6);
+    System.out.println(uniCode);
+    }
+    public static void main(String[] args){
+    short sVal = 10;
+    short result = (short)(sVal + 10);
+    System.out.println(result);
+    }
+  public static void main(String[] args) {
+    // float 타입 변수 선언과 초기화
+    float myFloat = 9.75f; // 숫자 뒤에 'f'를 붙임
+
+    // float를 사용한 계산
+    float anotherFloat = myFloat  2;
+
+    // 결과 출력
+    System.out.println("Original float value: " + myFloat);
+    System.out.println("Calculated float value: " + anotherFloat);
+  }
+    public static void main(String[] args){
+      boolean isture = false;
+
+    if (isture) {
+      System.out.println("이건트루입니다.");
+          } else {
+      System.out.println("이건펄스입니다.");
+    }
+      }
+  }**/
